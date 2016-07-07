d5bf2637d411c99fc7e5cdd9158fd9d8ab5c1e16
Soufiane Karrakchou <Soufiane Karrakchou>
2016-07-07 17:56:19 +0900

    add notifs

--- client/goal.js
+++ client/goal.js
@@ -1,5 +1,15 @@
 Template.goal.events ({
     'click .toggle-menu': function() { 
-        Meteor.call('toggleMenuItem', this._id, this.inList)
+        Meteor.call('toggleMenuItem', this._id, this.inList);
+    },
+    'click .notify': function() {    
+        var aspects = [];
+        _.each(this.positive_aspects, function(asp) {
+            aspects.push(asp.name);
+        });
+        var options = {
+            body: `Do not forget that "${this.goal_name}" will allow you to "${aspects[0]}"`,
+        };
+        Meteor.call('notifyMe', options);
     }
 });
--- collections/methodes.js
+++ collections/methodes.js
@@ -2,8 +2,18 @@ Meteor.methods({
     toggleMenuItem: function(id, currentstate) {
         Goals.update(id, {
             $set: {
-                inList: !currentstate
+                inList: !currentstate,
             }
-        })
+        });
+    },
+    
+    notifyMe: function(options) { 
+
+        perm = notify.permissionLevel();
+        if (perm == notify.PERMISSION_DEFAULT) {
+           notify.requestPermission();    
+        } else if (perm == notify.PERMISSION_GRANTED) {
+            var notif = new Notification('GoalBook',options);;
+        }
     }
 });
